6cc2386e86cee939ace7f21d731b44ee11bf0063
JeonSooMin <wjsaos2081@gmail.com>
2024-05-07 15:14:28 +0900

    fix: CORS 허용을 와일드카드(*)를 이용해 모든 출처를 허용할 경우 Access-Control-Allow-Credentials 를 true로 주지 못하도록 변경됨. 해당 사항을 고침.

--- src/main/java/com/oauth2/practices3/StompChatting/Config/WebCorsfig.java
+++ src/main/java/com/oauth2/practices3/StompChatting/Config/WebCorsfig.java
@@ -10,7 +10,8 @@ public class WebCorsfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+//            .allowedOrigins("http://localhost:3000")
+            .allowedOriginPatterns("http://localhost:3000/**")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false)
